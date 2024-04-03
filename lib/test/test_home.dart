7a307d7f2d25ab26e6bdd6edd061e409b79df59b
Le Thao <lethithao666666@gmail.com>
2024-04-03 23:30:45 +0700

    Test button

--- lib/test/test_home.dart
+++ lib/test/test_home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:volunteer_connection/test/test_item_notification.dart';
 import 'package:volunteer_connection/test/test_textfield_edit.dart';
+import 'package:volunteer_connection/test/test_button_login.dart';
 
 class TestHome extends StatefulWidget {
   const TestHome({super.key});
@@ -35,6 +36,17 @@ class _TestHomeState extends State<TestHome> {
                 ),
               ),
               child: const Text("Text item notification"),
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => const TestButtonLogin(
+                    nameButton: "Login",
+                  ),
+                ),
+              ),
+              child: const Text("Test button login"),
             )
           ],
         ),
