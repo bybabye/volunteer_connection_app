1c553ab99c6a0a7fc4c63ffecf787621d9b2a8af
bybabye <89548252+bybabye@users.noreply.github.com>
2024-05-23 10:26:49 +0700

    update socket

--- lib/features/chat/domain/entity/chat.dart
+++ lib/features/chat/domain/entity/chat.dart
@@ -4,12 +4,16 @@ class Chat {
   final String id;
   final String type;
   final List<User> members;
+  final List<User> administrator;
+  final List<User> pending;
   final String lastMess;
   final String avatar;
   final String name;
 
   Chat(
-      {required this.id,
+      {required this.administrator,
+      required this.pending,
+      required this.id,
       required this.type,
       required this.members,
       required this.lastMess,
@@ -18,10 +22,21 @@ class Chat {
 
   factory Chat.fromJson(Map<String, dynamic> json) {
     List<User> parsedMembers = [];
+    List<User> parsedAdministrator = [];
+    List<User> pending = [];
     if (json['members'] != null) {
       var memberList = json['members'] as List;
       parsedMembers =
           memberList.map((member) => User.fromJson(member)).toList();
+    }
+    if (json['administrator'] != null) {
+      var memberList = json['administrator'] as List;
+      parsedAdministrator =
+          memberList.map((member) => User.fromJson(member)).toList();
+    }
+    if (json['pending'] != null) {
+      var memberList = json['pending'] as List;
+      pending = memberList.map((member) => User.fromJson(member)).toList();
     }
 
     return Chat(
@@ -31,6 +46,8 @@ class Chat {
       lastMess: json['lastMess'] ?? "",
       avatar: json['avatar'] ?? "",
       name: json['nameGruop'] ?? "",
+      administrator: parsedAdministrator,
+      pending: pending,
     );
   }
 }
